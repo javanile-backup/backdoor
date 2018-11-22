1a31ed04e90f45c5c108095c61fa4571b45ea85f
Francesco Bianco <info.francescobianco@gmail.com>
2018-11-22 04:14:37 +0100

    add logs

--- foreground.sh
+++ foreground.sh
@@ -10,14 +10,14 @@ USER=${BACKDOOR_USER:-root}
 ## Run service as remote target
 if [ -n "${BACKDOOR_BIND}" ]; then
     echo "[BACKDOOR:INFO] 'Target' bind port '${BACKDOOR_BIND}' on remote server '${HOST}:${PORT}'."
-    backdoor bind ${BACKDOOR_BIND} ${HOST} ${PORT}
+    backdoor bind ${BACKDOOR_BIND} ${HOST}:${PORT}
     exit 2
 fi
 
 ## Run service as active client
 if [ -n "${BACKDOOR_OPEN}" ]; then
     echo "[BACKDOOR:INFO] 'Client' open port '${BACKDOOR_OPEN}' on remote server '${HOST}:${PORT}'."
-    backdoor open ${BACKDOOR_OPEN} ${HOST} ${PORT}
+    backdoor open ${BACKDOOR_OPEN} ${HOST}:${PORT}
     exit 3
 fi
 
