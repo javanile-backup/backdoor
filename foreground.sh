8bc67faaf18cb030bef786a0d457ff7ddc080b26
Francesco Bianco <info.francescobianco@gmail.com>
2018-11-22 04:46:32 +0100

    add logs

--- foreground.sh
+++ foreground.sh
@@ -9,18 +9,18 @@ USER=${BACKDOOR_USER:-root}
 
 ## Run service as remote target
 if [ -n "${BACKDOOR_BIND}" ]; then
-    echo "[BACKDOOR:INFO] 'Target' bind port '${BACKDOOR_BIND}' on remote server '${HOST}:${PORT}'."
+    echo "[BACKDOOR:INFO] Target bind port '${BACKDOOR_BIND}' on remote server '${HOST}:${PORT}'."
     backdoor bind ${BACKDOOR_BIND} ${HOST}:${PORT}
     exit 2
 fi
 
 ## Run service as active client
 if [ -n "${BACKDOOR_OPEN}" ]; then
-    echo "[BACKDOOR:INFO] 'Client' open port '${BACKDOOR_OPEN}' on remote server '${HOST}:${PORT}'."
+    echo "[BACKDOOR:INFO] Client open port '${BACKDOOR_OPEN}' on remote server '${HOST}:${PORT}'."
     backdoor open ${BACKDOOR_OPEN} ${HOST}:${PORT}
     exit 3
 fi
 
 ## Run service as daemon
-echo "[BACKDOOR:INFO] 'Server' listen on port '${PORT}'"
+echo "[BACKDOOR:INFO] Server listen on port '${PORT}'"
 /usr/sbin/sshd -D
