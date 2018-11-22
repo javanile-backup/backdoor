2ac962ac6d7393685d9a8154b7bfcc5d565b7e76
Francesco Bianco <info.francescobianco@gmail.com>
2018-11-22 04:44:59 +0100

    add logs

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-r="^([^:]*)(:([0-9]+))?$" && [[ "$1" =~ $r ]] && h="${BASH_REMATCH[1]}" && p="${BASH_REMATCH[3]}"
-HOST=
-
-echo $BASH_REMATCH
-echo "host: ${HOST}"
-echo "port: ${PORT}"
