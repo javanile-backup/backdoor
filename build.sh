394d0d71069cf14fbe394d261d6d9dbf7a56ab1c
Francesco Bianco <info.francescobianco@gmail.com>
2018-11-22 06:17:32 +0100

    build

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+##
+# javanile/backdoor (v0.0.1)
+# Reverse SSH tunnel for Docker
+##
+
 docker-compose down --remove-orphans
 docker-compose build
+docker-compose up
+
 git commit -am "build"
 git push
--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,7 @@ HOST=${BACKDOOR_HOST:-localhost}
 
 ## Run service as remote target
 if [ -n "${BACKDOOR_BIND}" ]; then
+    sleep 1
     echo "[BACKDOOR:INFO] Target bind port '${BACKDOOR_BIND}' on remote server '${HOST}:${PORT}'."
     backdoor bind ${BACKDOOR_BIND} ${HOST}:${PORT}
     exit 2
@@ -20,6 +21,7 @@ fi
 
 ## Run service as active client
 if [ -n "${BACKDOOR_OPEN}" ]; then
+    sleep 1
     echo "[BACKDOOR:INFO] Client open port '${BACKDOOR_OPEN}' on remote server '${HOST}:${PORT}'."
     backdoor open ${BACKDOOR_OPEN} ${HOST}:${PORT}
     exit 3
